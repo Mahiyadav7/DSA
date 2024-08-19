7f1e80f305b19fbbe7923756ec47763b359f7247
Akshit <mahi004tb@gmail.com>
2024-08-19 20:11:40 +0530

    Added ArrayInput class

---
+++ src/dsa/start/ArrayInput.java
@@ -0,0 +1,21 @@
+package dsa.start;
+
+import java.util.Scanner;
+
+public class ArrayInput {
+	
+	public void input() {
+		Scanner sc=new Scanner(System.in);
+		int array[] = new int[5];
+		System.out.println("Enter 5 numbers");
+		for(int i=0;i<5;i++) {
+			array[i]= sc.nextInt();
+		}
+		
+		System.out.println("Display the Array");
+		for(int i=0;i<5;i++) {
+			System.out.println(array[i]);
+		}
+	}
+
+}
--- src/dsa/start/MainDSA.java
+++ src/dsa/start/MainDSA.java
@@ -4,17 +4,8 @@ import java.util.*;
 public class MainDSA {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int array[] = new int[5];
-		System.out.println("Enter 5 numbers");
-		for(int i=0;i<5;i++) {
-			array[i]= sc.nextInt();
-		}
-		
-		System.out.println("Display the Array");
-		for(int i=0;i<5;i++) {
-			System.out.println(array[i]);
-		}
+		ArrayInput arrayObj = new ArrayInput();
+		arrayObj.input();
 	}
 
 }
